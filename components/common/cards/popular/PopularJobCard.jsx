3008baad9af01cafc4839f522f036c94b81bdbd2
ezgi <esgiefe@gmail.com>
2023-07-21 11:29:24 +0200

    updated searchpage and search func

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -3,9 +3,10 @@ import { View, Text, TouchableOpacity, Image } from "react-native";
 
 import styles from "./popularjobcard.style";
 
-import {checkImageURL} from '../../../../utils';
+import { checkImageURL } from "../../../../utils";
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
+
   return (
     <TouchableOpacity
       style={styles.container(selectedJob, item)}
--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -13,7 +13,9 @@ import styles from "./welcome.style";
 import { icons, SIZES } from "../../../constants";
 
 const jobsTypes = ["Full-time", "Part-time", "Contractor"];
-const Welcome = () => {
+
+
+const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   const router = useRouter();
   const [activeJobType, setActiveJobType] = useState("Full-time");
   return (
@@ -25,12 +27,12 @@ const Welcome = () => {
         <View style={styles.searchWrapper}>
           <TextInput
             style={styles.searchInput}
-            value=""
-            onChange={() => {}}
+            value={searchTerm}
+            onChangeText={(text) => setSearchTerm(text)}
             placeholder="What are you looking for?"
           />
         </View>
-        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+        <TouchableOpacity style={styles.searchBtn} onPress={handleClick}>
           <Image
             source={icons.search}
             resizeMode="contain"
@@ -43,7 +45,6 @@ const Welcome = () => {
           data={jobsTypes}
           renderItem={({ item }) => (
             <TouchableOpacity
-              
               style={styles.tab(activeJobType, item)}
               onPress={() => {
                 setActiveJobType(item);
@@ -54,7 +55,7 @@ const Welcome = () => {
             </TouchableOpacity>
           )}
           keyExtractor={(item) => item}
-          contentContainerStyle={{ columnGap: SIZES.small}}
+          contentContainerStyle={{ columnGap: SIZES.small }}
           horizontal
         />
       </View>
